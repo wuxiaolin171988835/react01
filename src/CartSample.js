fe2ba85d20d3b466186e4cfdccf4aa38929a14dc
wuxiaolin <wuxiaolin@gaosiedu.com>
2019-02-25 15:09:12 +0800

    Add cart num operate.

--- src/CartSample.js
+++ src/CartSample.js
@@ -1,5 +1,25 @@
 import React, { Component } from "react";
-
+function Cart(props) {
+  return (
+    <table>
+      <tbody>
+        {props.cart.map(item => (
+          <tr key={item.id}>
+            <td>{item.text}</td>
+            <td>
+              {/* 有指定参数需要添加箭头函数，无指定参数可以直接调用addGood */}
+              <button onClick={() => props.minusCount(item)}>-</button>
+              {item.num}
+              <button onClick={() => props.addCount(item)}>+</button>
+            </td>
+            <td>{item.price}</td>
+            <td>￥{item.num * item.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 class CartSample extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +47,9 @@ class CartSample extends Component {
       text: e.target.value
     });
   }
+  /**
+   * 新增商品
+   */
   addGood = () => {
     this.setState(prevState => ({
       goods: [
@@ -40,6 +63,9 @@ class CartSample extends Component {
       ]
     }));
   };
+  /**
+   * 添加至购物车
+   */
   addToCart = good => {
     const newCart = [...this.state.cart];
     const index = newCart.findIndex(item => item.text === good.text);
@@ -54,7 +80,32 @@ class CartSample extends Component {
       cart: newCart
     });
   };
-
+  /**
+   * 增加商品数量
+   */
+  addCount = good => {
+    const newCart = [...this.state.cart];
+    const index = newCart.findIndex(item => item.text === good.text);
+    const item = newCart[index];
+    newCart.splice(index, 1, { ...good, num: good.num + 1 });
+    this.setState({
+      cart: newCart
+    });
+  };
+  /**
+   * 减少商品
+   */
+  minusCount = good => {
+    const newCart = [...this.state.cart];
+    const index = newCart.findIndex(item => item.text === good.text);
+    const item = newCart[index];
+    if (good.num > 1) {
+      newCart.splice(index, 1, { ...good, num: good.num - 1 });
+      this.setState({
+        cart: newCart
+      });
+    }
+  };
   render() {
     const { goods, cart } = this.state;
     const goodsList = goods.map(item => {
@@ -73,18 +124,13 @@ class CartSample extends Component {
         <button onClick={this.addGood}>添加商品</button>
         <ul>{goodsList}</ul>
         {this.props.title && <h1>{this.props.title}</h1>}
-        <table>
-          <tbody>
-            {cart.map(item => {
-              return (
-                <tr key={item.id}>
-                  <td>{item.text}</td>
-                  <td>{item.num}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {
+          <Cart
+            cart={cart}
+            addCount={this.addCount}
+            minusCount={this.minusCount}
+          />
+        }
       </div>
     );
   }
